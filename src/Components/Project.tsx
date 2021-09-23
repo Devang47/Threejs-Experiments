dceaf3657fdc90b3f0f5c0f3b535cf192fb8baa0
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-09-23 12:38:15 +0530

    updated

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FiExternalLink } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
 
 interface props {
   projectHead: string;
@@ -16,8 +18,24 @@ function Project({
   return (
     <article className="project_wrapper shadow">
       <div className="details">
-        <h2 className="font_2">{projectHead}</h2>
+        <h2 className="font_2 ">{projectHead}</h2>
         <p className="font_2">{projectDetails}</p>
+        <div className="links">
+          <a
+            href={projectLinks.githubRepoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href={projectLinks.projectLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FiExternalLink />
+          </a>
+        </div>
       </div>
 
       <img src={coverImg} alt="project-1" />
--- src/Components/ProjectsData.ts
+++ src/Components/ProjectsData.ts
@@ -2,8 +2,7 @@ const ProjectsData = [
   {
     id: 1,
     projectName: "Super awesome project here.",
-    projectDetails:
-      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum velit sunt dolorum illum dolorem piditate maiores blanditiis quas, tenetur voluptatibus asperiores incidunt, sit animi totam quam iste. Eos, sequi.",
+    projectDetails: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum velit sunt dolorum illum dolorem piditate maiores blanditiis quas, tenetur voluptatibus asperiores incidunt, sit animi totam quam iste. Eos, sequi.`,
     coverImg:
       "https://images.unsplash.com/photo-1632295625247-f3b6e4dbcec5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     links: {
@@ -14,8 +13,7 @@ const ProjectsData = [
   {
     id: 2,
     projectName: "Super awesome project here.",
-    projectDetails:
-      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum velit sunt dolorum illum dolorem piditate maiores blanditiis quas, tenetur voluptatibus asperiores incidunt, sit animi totam quam iste. Eos, sequi.",
+    projectDetails: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum velit sunt dolorum illum dolorem piditate maiores blanditiis quas, tenetur voluptatibus asperiores incidunt, sit animi totam quam iste. Eos, sequi.`,
     coverImg:
       "https://images.unsplash.com/photo-1632295625247-f3b6e4dbcec5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     links: {
@@ -26,8 +24,7 @@ const ProjectsData = [
   {
     id: 3,
     projectName: "Super awesome project here.",
-    projectDetails:
-      " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum velit sunt dolorum illum dolorem piditate maiores blanditiis quas, tenetur voluptatibus asperiores incidunt, sit animi totam quam iste. Eos, sequi.",
+    projectDetails: ` Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto earum velit sunt dolorum illum dolorem piditate maiores blanditiis quas, tenetur voluptatibus asperiores incidunt, sit animi totam quam iste. Eos, sequi.`,
     coverImg:
       "https://images.unsplash.com/photo-1632295625247-f3b6e4dbcec5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80",
     links: {
