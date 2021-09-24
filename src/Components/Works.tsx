07c3ba3024a34aa7f9c19575b10e5a5877114c13
Devang Saklani <72780418+Devang47@users.noreply.github.com>
2021-09-24 13:42:06 +0530

    working on css

--- src/Components/Works.tsx
+++ src/Components/Works.tsx
@@ -1,27 +1,54 @@
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import Project from "./Project";
 import ProjectsData from "./ProjectsData";
 
-function Works() {
-  return (
-    <section id='showcase'>
-      <div className="wrapper">
+import gsap from "gsap";
 
-      <h2 className="section_head font_2">Recent Works:</h2>
-      <div className="projects">
-        {ProjectsData.map((data) => {
-          return (
-            <Project
-            projectHead={data.projectName}
-            key={data.id}
-            projectDetails={data.projectDetails}
-            coverImg={data.coverImg}
-            projectLinks={data.links}
-            />
+function Works() {
+  const objects = useRef(null);
+  const allCircles = gsap.utils.selector(objects);
+
+  useLayoutEffect(() => {
+    window.addEventListener("mousemove", animateCircle);
+    return () => {
+      window.removeEventListener("mousemove", animateCircle);
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  function animateCircle(event: any) {
+    let mouseX = event.clientX / window.innerWidth - 0.5;
+    let mouseY = event.clientY / window.innerHeight - 0.5;
+
+    gsap.to(allCircles(".object"), {
+      x: 50 * mouseX,
+      y: 50 * mouseY,
+      duration: 0.5,
+    });
+  }
+
+  return (
+    <section id="showcase" ref={objects}>
+      <div className="wrapper">
+        <h2 className="section_head font_2">Recent Works:</h2>
+        <div className="projects">
+          {ProjectsData.map((data) => {
+            return (
+              <Project
+                projectHead={data.projectName}
+                key={data.id}
+                projectDetails={data.projectDetails}
+                coverImg={data.coverImg}
+                projectLinks={data.links}
+              />
             );
           })}
+        </div>
       </div>
-          </div>
+
+      <div className="circle1 object"></div>
+      <div className="circle2 object"></div>
+      <div className="cube object"></div>
     </section>
   );
 }
